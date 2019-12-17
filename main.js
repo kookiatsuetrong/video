f7ec4d8f689d6bf33ae14a134a1e57ded0827c7a
Kookiat <kookiatsuetrong@gmail.com>
2019-12-17 19:53:49 +0700

    Keep Walking

--- main.js
+++ main.js
@@ -5,11 +5,24 @@ var source  = { host: 'localhost', database: 'webschool',
                 user: 'james',     password: 'bond' }
 var pool    = mysql.createPool(source)
 var server  = express()   // create server
+var reader  = express.urlencoded({extended:false})
 server.listen(2000)
 server.engine('html', ejs.renderFile)
 
 server.get('/', showHome)
+server.get('/search', showSearch)
+server.get ('/login',  showLogIn)
+server.post('/login',  reader, checkLogIn)
 server.use(express.static('public'))
+
+function showLogInt(req, res) { 
+    res.render('login.html') 
+}
+
+function checkLogIn(req, res) {
+    // req.body.email,        req.body.secret
+    res.send(req.body)
+}
 
 function showHome(req, res) {
     var sql = "select * from lesson where type='free'"
@@ -19,3 +32,19 @@ function showHome(req, res) {
         res.render('index.html', model)
     })
 }
+
+function showSearch(req, res) {
+    var model = { }
+    if (req.query.video == null) {
+        model.result = [ ]
+        res.render('search.html', model)
+    } else {
+        var sql = 'select * from lesson where title like ? or detail like ?'
+        var data = [ '%' + req.query.video + '%', '%' + req.query.video + '%' ]
+        pool.query(sql, data, function(error, data) {
+            model.result = data
+            res.render('search.html', model)
+        })
+    }
+}
+
